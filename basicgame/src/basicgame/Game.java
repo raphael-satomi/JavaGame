9972a6a1d48e177417b5c1e8816b4d6bb0e49562
Raphael Satomi <raphaelsatomi@gmail.com>
2022-11-02 04:37:27 -0300

    Movendo bloco

--- basicgame/src/basicgame/Game.java
+++ basicgame/src/basicgame/Game.java
@@ -1,16 +1,46 @@
 package basicgame;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable, KeyListener{
 
 	public static int WIDTH = 480, HEIGHT = 480;
+	public Player player;
 	
 	public Game() {
+		this.addKeyListener(this);
 		this.setPreferredSize( new Dimension(WIDTH, HEIGHT) );
+		
+		player = new Player(0, 0);
+	}
+	
+	public void tick() { //Lógica do jogo (movimentação, colisões, etc...)
+		player.tick();
+	}
+	
+	public void render() { //Renderizar gráficos
+		BufferStrategy bs = this.getBufferStrategy();
+		if( bs == null ) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.gray);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		player.render(g);
+		
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
@@ -33,9 +63,61 @@ public class Game extends Canvas implements Runnable{
 		
 		while(true) {
 			//Código jogo
+			tick();
+			render();
+			
+			try {
+				Thread.sleep(1000/60); //Rode 60fps
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}	
 		}
 	}
 	
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		if( e.getKeyCode() == KeyEvent.VK_RIGHT ) {
+			player.right = true;
+			
+		}else if( e.getKeyCode() == KeyEvent.VK_LEFT ) {
+			player.left = true;
+		}
+		
+		if( e.getKeyCode() == KeyEvent.VK_UP ) {
+			player.up = true;
+			
+		}else if( e.getKeyCode() == KeyEvent.VK_DOWN ) {
+			player.down = true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if( e.getKeyCode() == KeyEvent.VK_RIGHT ) {
+			player.right = false;
+			
+		}else if( e.getKeyCode() == KeyEvent.VK_LEFT ) {
+			player.left = false;
+		}
+		
+		if( e.getKeyCode() == KeyEvent.VK_UP ) {
+			player.up = false;
+			
+		}else if( e.getKeyCode() == KeyEvent.VK_DOWN ) {
+			player.down = false;
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
 
 
